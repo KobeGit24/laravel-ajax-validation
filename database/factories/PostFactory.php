15a0213f09a4b58637f26b5f0ae293b534fb5f91
ValerioTrinca <valer.tria@gmail.com>
2020-10-08 14:10:46 +0200

    Migrations

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,10 +3,14 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model;
+use App\Post;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Post::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker -> word,
+        'description' => $faker -> realText(),
+        'like' => $faker -> randomDigit,
+        'dislike' => $faker -> randomDigit
     ];
 });
